e2c32c1d4b430ec1f87b8eb3f287085d02690ab8
sulaimanade <98491717+sulaimanade@users.noreply.github.com>
2024-10-18 18:31:04 +0100

    Create input.py

---
+++ lesson/input.py
@@ -0,0 +1,7 @@
+#INPUT function: allows users to input value
+name = input("what is your name?")
+Age = input("how old are you?")
+print("welcome, " + name + ", you are " + Age + " years old")
+print("please confirm your name and age are correct, by typing Y for yes or N for no")
+confirm = input("")
+print("Thank you, your transaction is now processing!")
