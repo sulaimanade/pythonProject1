6d6c5002d6fa25c50d215d7da69f679c7925f142
sulaimanade <98491717+sulaimanade@users.noreply.github.com>
2024-10-19 08:59:41 +0100

    Update variables.py

--- lesson/variables.py
+++ lesson/variables.py
@@ -15,3 +15,13 @@ print(home_address + " is the location") #printing the variable
 
 phrase = "Life is good" #variable
 print(phrase + ", make it count") #variable with concatenation
+
+print(phrase.lower()) #This is a function, this function renders the variable as lower case
+print(phrase.upper()) #This is a function, this function renders the variable as UPPER case
+print(phrase.isupper()) #This is a function, this function checks if the variable is in UPPER case
+print(phrase.islower()) #This is a function, this function checks if the variable is in lower case
+print(phrase.upper().isupper()) #This is a multi-function, first function converts to UPPER, second checks if the variable is in UPPER case
+print(len(phrase)) #This is a function that count & prints the number of characters in the variable phrase
+print(phrase[6]) #This is a function that identify & returns character in the position stated
+print(phrase.index("e")) #This is a function that identify & returns the position of a specified character
+print(phrase.replace("good", "beautiful"))
