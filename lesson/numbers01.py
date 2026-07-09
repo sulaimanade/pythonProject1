8e2290354296a1cb132eb3dd80e8689eec7e98f6
sulaimanade <98491717+sulaimanade@users.noreply.github.com>
2026-07-09 08:45:47 +0100

    Update numbers01.py

--- lesson/numbers01.py
+++ lesson/numbers01.py
@@ -1,4 +1,4 @@
-from math import *  #this will import the math module
+from math import *  #import the math module
 print(floor(2.6)) #approximate like round down the number
 print(ceil(2.1)) #approximate like round up the number, provided digit after decimal is higher than zero
 print(sqrt(25))
